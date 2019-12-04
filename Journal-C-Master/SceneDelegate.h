4d3b5a6d425dfdb3faa8653a39e85bdfd73b240c
Chris Anderson <cda84097@gmail.com>
2019-12-04 16:43:56 -0700

    Initial Commit

---
+++ Journal-C-Master/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Journal-C-Master
+//
+//  Created by Chris Anderson on 12/4/19.
+//  Copyright © 2019 Renaissance Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Journal-C-Master/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Journal-C-Master
+//
+//  Created by Chris Anderson on 12/4/19.
+//  Copyright © 2019 Renaissance Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
